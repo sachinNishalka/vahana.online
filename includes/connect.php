a2356ede0d13bfba58f44a706e4d1fa836bcd5fc
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2022-12-31 22:21:44 +0530

    database update

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$con=mysqli_connect('localhost','root','','mystore');
+$con=mysqli_connect('localhost','u996882231_geevinda','Geevinda123','u996882231_mystore');
 if(!$con){
     die(mysqli_error($con));
 }
