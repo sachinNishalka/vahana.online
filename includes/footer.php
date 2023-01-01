40029f10b23bc2c2aa173046d19271460f668d3f
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2023-01-01 12:38:18 +0530

    removed

--- includes/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="bg-secondary p-2 text-center">
-  <p>All Rights Reserved @ - Designed by Vahana-2022</p>
- </div>
--- session/sessions.php
+++
@@ -1,9 +0,0 @@
-<?php
-//sessions()
-session_start();
-$_SESSION['username']="Ayana";
-$_SESSION['password']="123";
-$_SESSION['email']="ayan@gmail.com";
-echo"Session data is saved";
-
-?>
